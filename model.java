605698f081efb13bf55361b6ccaff81f1c78af70
iamrafal1 <84661535+iamrafal1@users.noreply.github.com>
2021-12-05 19:58:37 +0000

    Add files via upload
    Model.java will test inputs given by test.java. The first test will be to see if there is an @ in the provided email. Currently doesn't support this.

---
+++ model.java
@@ -0,0 +1,10 @@
+package test;
+
+public class model {
+    private String[] special = {"@", ".", "&", "*", "!", "^"};
+
+    public static Boolean checkEmail(String email){
+        return true;
+    }
+
+}
