d76ed70d168955ba0b0c5ff0a0bf467d3f781ccc
iamrafal1 <84661535+iamrafal1@users.noreply.github.com>
2021-12-05 20:17:08 +0000

    Add files via upload
    test to check if email contains @ and ".". Test successful. Next test checks if the @ occurs before the "."

--- model.java
+++ model.java
@@ -4,7 +4,7 @@ public class model {
     private static String[] special = {"@", ".", "&", "*", "!", "^"};
 
     public static Boolean checkEmail(String email){
-        if (email.contains(special[0])){
+        if (email.contains(special[0]) && email.contains(special[1])){
             return true;
         }
 
